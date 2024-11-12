95fc21deaeb2f406a35620efa3f8c1f84a0d3b1e
Mukonyole Evans <emukonyole@gmail.com>
2024-11-12 09:43:40 +0000

    Task 1

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
 
-const wrapper = shallow(<Header />);
-describe("Header component tests", () => {
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Header", () => {
   it("render without crashing", () => {
+    const wrapper = shallow(<Header />);
     expect(wrapper.exists()).toEqual(true);
   });
   it("should render a h1", () => {
+    const wrapper = shallow(<Header />);
     expect(wrapper.exists("img")).toEqual(true);
     expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toEqual(true);
   });
