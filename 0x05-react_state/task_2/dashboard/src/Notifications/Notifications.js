d55b6a14acc5af6e1701d83484f4fe85c214c168
Mukonyole Evans <emukonyole@gmail.com>
2024-11-18 09:35:45 +0000

    task 2

---
+++ 0x05-react_state/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Notifications extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.displayDrawer !== this.props.displayDrawer;
+  }
+
+  render() {
+    const { displayDrawer, handleDisplayDrawer, handleHideDrawer } = this.props;
+    return (
+      <div>
+        <div onClick={handleDisplayDrawer} className="menuItem">
+          Your notifications
+        </div>
+        {displayDrawer && (
+          <div className="Notifications">
+            <button
+              onClick={handleHideDrawer}
+              aria-label="Close"
+            >
+              Close
+            </button>
+            <p>Here is the list of notifications</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func,
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {},
+};
+
+export default Notifications;
